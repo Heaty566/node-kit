bf9ba54814273c0a0ab5ac71830352b5a5396d6f
heaty566 <heaty566@gmail.com>
2021-04-08 16:35:40 +0700

    new feature

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -1,10 +1,7 @@
 declare global {
         namespace NodeJS {
                 interface ProcessEnv {
-                        SESSION_SECRET: string;
-                        NODE_ENV: "development" | "production";
-                        DB_URL: string;
-                        CLIENT_URL: string | string[];
+                        [key: string]: string;
                 }
         }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,32 +3,32 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import mongodbSession from "connect-mongodb-session";
 import session from "express-session";
-import { config } from "../config";
+
 import morgan from "morgan";
 const MongoDbStore = mongodbSession(session);
 const sessionStore = new MongoDbStore({
-        uri: config.DB_URL,
+        uri: process.env.DB_URL,
         collection: "session",
-        expires: config.day * 30,
+        expires: 86400 * 30,
 });
 
 export const routers = (app: Express) => {
         app.use(json());
-        app.use(cors({ origin: config.CLIENT_URL, credentials: true }));
+        app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
         app.use(morgan("dev"));
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
         app.use(express.static(process.cwd() + "/public"));
         //main routers
         app.use(
                 session({
-                        secret: config.SESSION_SECRET,
+                        secret: process.env.SESSION_SECRET,
                         name: "sessionId",
                         resave: true,
                         saveUninitialized: true,
                         store: sessionStore,
                         cookie: {
-                                maxAge: config.day * 30,
+                                maxAge: 86400 * 30,
                         },
                 })
         );
