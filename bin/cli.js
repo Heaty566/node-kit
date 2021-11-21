8f215b4f9299aabfa126f5ea5613a988a7cff352
heaty566 <heaty566@gmail.com>
2021-11-21 13:47:01 +0700

    1.1.0

--- bin/cli.js
+++ bin/cli.js
@@ -1,25 +1,28 @@
-#!/user/bin/env node
-const { execSync } = require('child_process');
-// const runCommand = (cmd) => {
-//       try {
-//             execSync(cmd, { stdio: 'inherit' });
-//       } catch (error) {
-//             console.log(`Failed to execute ${error}`);
-//             return false;
-//       }
+#!/usr/bin/env node
 
-//       return true;
-// };
+const { execSync } = require('child_process');
+const runCommand = (cmd) => {
+      try {
+            execSync(cmd, { stdio: 'inherit' });
+      } catch (error) {
+            console.log(`Failed to execute ${error}`);
+            return false;
+      }
+
+      return true;
+};
 
 console.log('Cloning the repository');
-// const repoName = process.argv[2];
+const repoName = process.argv[2];
 
-// const gitCheckoutCommand = `git clone --depth 1 https://github.com/MonoInfinity/mono-node-kit-ts.git ${repoName}`;
-// const installDepsCommand = `cd ${repoName} && npm install yarn -g && yarn install`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/MonoInfinity/mono-node-kit-ts.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install win-node-env -g && npm install yarn -g && yarn install`;
 
-// const checkoutCmd = runCommand(gitCheckoutCommand);
-// if (!checkoutCmd) process.exit(-1);
+const checkoutCmd = runCommand(gitCheckoutCommand);
+if (!checkoutCmd) process.exit(-1);
 
-// console.log('Install all dependencies');
-// const installCmd = runCommand(installDepsCommand);
-// if (!installCmd) process.exit(-1);
+console.log('Install all dependencies');
+const installCmd = runCommand(installDepsCommand);
+if (!installCmd) process.exit(-1);
+console.log('----------------------------------------------');
+console.log(`%c Let's create your awesome server 🍀🍀🍀 `, 'color: #047867');
